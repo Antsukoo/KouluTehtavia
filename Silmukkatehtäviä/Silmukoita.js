0e2f0acb50c91973641d5e24a1096af1b1a53ee5
unknown <anton.kuusisto@edu.keuda.fi>
2020-09-23 10:45:40 +0300

    Tässä HipHeijaa

--- Silmukkatehtäviä/Silmukoita.js
+++ Silmukkatehtäviä/Silmukoita.js
@@ -46,11 +46,11 @@ function HipHeijaa(){
   var numroJono = "";
 
   for (var i = 1; i < 100; i++) {
-    if (i % 3 == 0 && i % 6 != 0){
+    if (i % 3 == 0 && i % 5 != 0){
       numroJono += "Hip ";
-    }else if (i % 6 == 0 && i % 3 != 0){
+    }else if (i % 5 == 0 && i % 3 != 0){
       numroJono += "Heijaa ";
-    }else if (i % 6 == 0 && i % 3 == 0) {
+    }else if (i % 5 == 0 && i % 3 == 0) {
       numroJono += "Hip Heijaa ";
     }else{
       numroJono += i + " ";
