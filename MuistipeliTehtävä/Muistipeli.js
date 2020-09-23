e6c3dec9a0cd4f3eb02b71d0e109633eba37eb85
unknown <anton.kuusisto@edu.keuda.fi>
2020-09-23 11:28:16 +0300

    Ja vielä tämä

--- MuistipeliTehtävä/Muistipeli.js
+++ MuistipeliTehtävä/Muistipeli.js
@@ -1,8 +1,12 @@
 function Aloita(){
   alert('Heippa.');
 
+
+  var kortKuvat = []
   var kortit = document.createElement('table');
   var kortitTxt = document.createTextNode('Kortit');
+
+
 
   document.getElementById('Taulukko').innerHTML = kortit;
 }
